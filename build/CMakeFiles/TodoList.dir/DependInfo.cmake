69f34a1413de8d05e6c1f7175234ddc7504eab29
mrr0b0t1907 <mrrootabdessamad@gmail.com>
2026-05-23 18:22:54 +0100

    sort fixed

---
+++ build/CMakeFiles/TodoList.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/abdessamad/Todolist-project/src/FileManager.cpp" "CMakeFiles/TodoList.dir/src/FileManager.cpp.o" "gcc" "CMakeFiles/TodoList.dir/src/FileManager.cpp.o.d"
+  "/home/abdessamad/Todolist-project/src/PersonalTask.cpp" "CMakeFiles/TodoList.dir/src/PersonalTask.cpp.o" "gcc" "CMakeFiles/TodoList.dir/src/PersonalTask.cpp.o.d"
+  "/home/abdessamad/Todolist-project/src/Task.cpp" "CMakeFiles/TodoList.dir/src/Task.cpp.o" "gcc" "CMakeFiles/TodoList.dir/src/Task.cpp.o.d"
+  "/home/abdessamad/Todolist-project/src/TaskManager.cpp" "CMakeFiles/TodoList.dir/src/TaskManager.cpp.o" "gcc" "CMakeFiles/TodoList.dir/src/TaskManager.cpp.o.d"
+  "/home/abdessamad/Todolist-project/src/UI.cpp" "CMakeFiles/TodoList.dir/src/UI.cpp.o" "gcc" "CMakeFiles/TodoList.dir/src/UI.cpp.o.d"
+  "/home/abdessamad/Todolist-project/src/WorkTask.cpp" "CMakeFiles/TodoList.dir/src/WorkTask.cpp.o" "gcc" "CMakeFiles/TodoList.dir/src/WorkTask.cpp.o.d"
+  "/home/abdessamad/Todolist-project/src/main.cpp" "CMakeFiles/TodoList.dir/src/main.cpp.o" "gcc" "CMakeFiles/TodoList.dir/src/main.cpp.o.d"
+  "" "TodoList" "gcc" "CMakeFiles/TodoList.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/TodoList.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TodoList.dir/link.d"
+  "CMakeFiles/TodoList.dir/src/FileManager.cpp.o"
+  "CMakeFiles/TodoList.dir/src/FileManager.cpp.o.d"
+  "CMakeFiles/TodoList.dir/src/PersonalTask.cpp.o"
+  "CMakeFiles/TodoList.dir/src/PersonalTask.cpp.o.d"
+  "CMakeFiles/TodoList.dir/src/Task.cpp.o"
+  "CMakeFiles/TodoList.dir/src/Task.cpp.o.d"
+  "CMakeFiles/TodoList.dir/src/TaskManager.cpp.o"
+  "CMakeFiles/TodoList.dir/src/TaskManager.cpp.o.d"
+  "CMakeFiles/TodoList.dir/src/UI.cpp.o"
+  "CMakeFiles/TodoList.dir/src/UI.cpp.o.d"
+  "CMakeFiles/TodoList.dir/src/WorkTask.cpp.o"
+  "CMakeFiles/TodoList.dir/src/WorkTask.cpp.o.d"
+  "CMakeFiles/TodoList.dir/src/main.cpp.o"
+  "CMakeFiles/TodoList.dir/src/main.cpp.o.d"
+  "TodoList"
+  "TodoList.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TodoList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
