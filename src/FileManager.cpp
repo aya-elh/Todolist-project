04c382d87b15c669fde4bb9253eada248d1e0619
mrr0b0t1907 <mrrootabdessamad@gmail.com>
2026-05-23 11:32:55 +0100

    Update FileManager.cpp

--- src/FileManager.cpp
+++ src/FileManager.cpp
@@ -1,40 +1,134 @@
-#include "FileManager.h"
-#include "WorkTask.h"
-#include "PersonalTask.h"
+#include "../include/FileManager.h"
+#include "../include/WorkTask.h"
+#include "../include/PersonalTask.h"
+
 #include <fstream>
 #include <sstream>
+#include <iostream>
 
-void FileManager::save(const std::string& filename, TaskManager& manager) {
+
+void FileManager::save(
+    const std::string& filename,
+    TaskManager& manager
+) {
+
     std::ofstream file(filename);
 
-    for (auto task : manager.getTasks()) {
-        file << task->getType() << ";"
-             << task->getTitle() << ";"
-             << task->getCategory() << ";"
-             << task->getPriority() << ";"
-             << task->getStatus() << "\n";
+    if (!file) {
+
+        std::cout
+            << "\nError opening file.\n";
+
+        return;
     }
+
+
+    for (Task* task : manager.getTasks()) {
+
+        file
+
+            << task->getType()
+            << ";"
+
+            << task->getTitle()
+            << ";"
+
+            << task->getCategory()
+            << ";"
+
+            << task->getPriority()
+            << ";"
+
+            << task->getStatus()
+
+            << "\n";
+    }
+
+
+    file.close();
+
+    std::cout
+        << "\nTasks saved successfully.\n";
 }
 
-void FileManager::load(const std::string& filename, TaskManager& manager) {
+
+
+void FileManager::load(
+    const std::string& filename,
+    TaskManager& manager
+) {
+
     std::ifstream file(filename);
+
+    if (!file) {
+
+        std::cout
+            << "\nNo saved file found.\n";
+
+        return;
+    }
+
+
     std::string line;
 
+
     while (getline(file, line)) {
+
         std::stringstream ss(line);
-        std::string type, title, category, status;
+
+        std::string type;
+        std::string title;
+        std::string category;
+        std::string status;
+
         int priority;
 
+
         getline(ss, type, ';');
+
         getline(ss, title, ';');
+
         getline(ss, category, ';');
+
+
         ss >> priority;
+
         ss.ignore();
+
+
         getline(ss, status);
 
-        if (type == "Work")
-            manager.addTask(new WorkTask(title, category, priority, status));
-        else
-            manager.addTask(new PersonalTask(title, category, priority, status));
+
+        if (type == "Work") {
+
+            manager.addTask(
+
+                new WorkTask(
+                    title,
+                    category,
+                    priority,
+                    status
+                )
+            );
+        }
+
+        else {
+
+            manager.addTask(
+
+                new PersonalTask(
+                    title,
+                    category,
+                    priority,
+                    status
+                )
+            );
+        }
     }
+
+
+    file.close();
+
+    std::cout
+        << "\nTasks loaded successfully.\n";
 }
